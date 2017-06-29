e1a08fe9eeb45d344ad35862929a50dc8363f989
Arthur L <mardybm@gmail.com>
2017-06-29 16:46:10 +0600

    add a job to render chat messages and broadcast them to room channel

---
+++ app/jobs/message_broadcast_job.rb
@@ -0,0 +1,16 @@
+class MessageBroadcastJob < ApplicationJob
+  queue_as :default
+
+  def perform(message)
+    ActionCable.server.broadcast 'room_channel', message: render_message(message)
+  end
+
+  private
+
+  def render_message(message)
+    ApplicationController.renderer.render(
+      partial: 'messages/message',
+      locals: { message: message }
+    )
+  end
+end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,7 @@
 class Message < ApplicationRecord
+  after_create_commit do
+    MessageBroadcastJob.perform_later(self)
+  end
+
   validates :content, presence: true
 end
